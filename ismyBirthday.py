b4671623044ea9dbeacd86013b0e4c0b691b55a7
mrohith29 <mariyalarohith29@gmail.com>
2024-03-03 21:18:43 +0530

    update

--- ismyBirthday.py
+++ ismyBirthday.py
@@ -1,28 +1,19 @@
-import datetime
-
 from flask import Flask, render_template
+import datetime
+from flask_frozen import Freezer
 
 ismyBirthday = Flask(__name__)
+freezer = Freezer(ismyBirthday)
 
 @ismyBirthday.route('/')
-
-def index():
+def home():
     now = datetime.datetime.now()
     mybirthday = now.month == 11 and now.day == 29
     return render_template('index.html', mybirthday=mybirthday)
 
 @ismyBirthday.route('/date')
-
 def date():
     return render_template('date.html')
 
-@ismyBirthday.route('/')
-def home():
-    return render_template('index.html')
-
-from flask_frozen import Freezer
-
-freezer = Freezer(ismyBirthday)
-
 if __name__ == '__main__':
-    freezer.freeze()
+    ismyBirthday.run(debug=True)
