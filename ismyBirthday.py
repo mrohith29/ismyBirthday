bdabf01c6fcda23fe156cc1cc365359f87e9e5b7
mrohith29 <mariyalarohith29@gmail.com>
2024-03-03 11:03:21 +0530

    add templates/index.html

--- ismyBirthday.py
+++ ismyBirthday.py
@@ -9,9 +9,4 @@ ismyBirthday = Flask(__name__)
 def index():
     now = datetime.datetime.now()
     mybirthday = now.month == 11 and now.day == 29
-    # if mybirthday:
-    #     return render_template('index.html', heading="Yes, it's my birthday!")
-    # else:
-    #     return render_template('index.html', heading="No, it's not my birthday.")
-
     return render_template('index.html', mybirthday=mybirthday)
