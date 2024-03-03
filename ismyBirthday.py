07b2e7e2f13c15b75518cf35f789901c15ffbb5b
mrohith29 <mariyalarohith29@gmail.com>
2024-03-03 21:02:46 +0530

    add frozen flask

--- ismyBirthday.py
+++ ismyBirthday.py
@@ -10,3 +10,19 @@ def index():
     now = datetime.datetime.now()
     mybirthday = now.month == 11 and now.day == 29
     return render_template('index.html', mybirthday=mybirthday)
+
+@ismyBirthday.route('/date')
+
+def date():
+    return render_template('date.html')
+
+@ismyBirthday.route('/')
+def home():
+    return render_template('index.html')
+
+from flask_frozen import Freezer
+
+freezer = Freezer(ismyBirthday)
+
+if __name__ == '__main__':
+    freezer.freeze()
